5b262c0fb85146b5730d14d777cd2727a1b339c1
Gilson <gilson@codemastersolucoes.com>
2018-05-30 16:31:22 -0300

    First commit application

---
+++ app/ModelsCategory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace WhatsShop;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ModelsCategory extends Model
+{
+    //
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WhatsShop\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'WhatsShop\Model' => 'WhatsShop\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
